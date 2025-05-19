7bd9ffe068cf8de27b256fd88645d1923180aa58
ILIAS BAHER <136027923+ILIASB1212@users.noreply.github.com>
2025-05-20 01:25:55 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+# Install system deps
+apt-get update && apt-get install -y $(cat apt-packages.txt)
 
+# Special numpy install first
+python -m pip install --upgrade "numpy==1.24.4" --only-binary numpy
+
+# Install remaining requirements
+python -m pip install -r requirements.txt --no-build-isolation
